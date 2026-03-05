32fbd910e768c4347020351c4257effb5cd4e160
chantree <juan.carlos.chantre@correounivalle.edu.co>
2026-03-05 09:43:04 -0500

    fix: correcion de worker

--- worker_runt.py
+++ worker_runt.py
@@ -1,27 +1,36 @@
 import requests
 import time
-from logica.api_runt import consultar_runt
 
 VPS_URL = "http://159.89.44.99:8000/cola"
+RUNT_URL = "http://127.0.0.1:8000/runt/validar"
 
 print("Worker iniciado...")
 
 while True:
+
     try:
-        r = requests.get(VPS_URL)
+
+        # preguntar al VPS si hay placa en cola
+        r = requests.get(VPS_URL, timeout=10)
         data = r.json()
 
         placa = data.get("placa")
 
         if placa:
+
             print("Procesando placa:", placa)
 
-            consultar_runt(placa)
+            # llamar al backend local que hace el scraping
+            respuesta = requests.get(f"{RUNT_URL}/{placa}", timeout=600)
+
+            print("Respuesta RUNT:", respuesta.json())
 
         else:
+
             print("No hay tareas")
 
     except Exception as e:
+
         print("Error:", e)
 
     time.sleep(5)
