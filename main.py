57dcfd7099718fac8b204dbf6a5b5c1c8c7d97da
chantree <juan.carlos.chantre@correounivalle.edu.co>
2026-03-08 22:20:28 -0500

    fix: correcion de cors

--- main.py
+++ main.py
@@ -17,12 +17,7 @@ BASE_PATH = os.getenv("BASE_PATH", "/home/backend/vehiculos")
 # ✅ CORS (MUY IMPORTANTE)
 app.add_middleware(
     CORSMiddleware,
-    allow_origins=[
-    "http://localhost:3000",
-    "http://127.0.0.1:3000",
-    "https://formulario-transporte-nueva-colombi.vercel.app",
-    "https://v0-resume-form-design.vercel.app"
-    ],
+    allow_origin_regex="https://.*\.vercel\.app",
     allow_credentials=True,
     allow_methods=["*"],
     allow_headers=["*"],
