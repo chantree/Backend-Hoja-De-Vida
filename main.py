eea26d98a064b3ca7f94ebac2faaefe1a6775f29
chantree <juan.carlos.chantre@correounivalle.edu.co>
2026-05-20 22:04:34 -0500

    fix: correcion de guardado

--- main.py
+++ main.py
@@ -21,7 +21,7 @@ BASE_PATH = os.getenv("BASE_PATH", "/home/backend/vehiculos")
 app.add_middleware(
     CORSMiddleware,
     allow_origin_regex="https://.*\.vercel\.app",
-    allow_credentials=True,
+    allow_credentials=False,
     allow_methods=["*"],
     allow_headers=["*"],
 )
